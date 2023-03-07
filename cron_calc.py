3088e421007d7d8dbbb776eca06c5d087f7f3ee9
Vido <956388+Vido@users.noreply.github.com>
2023-03-07 00:41:09 -0300

    Improve binance calc

--- cron_calc.py
+++ cron_calc.py
@@ -28,20 +28,24 @@ from bot import send_msg
 
 ibrx_tickers = [ "%s.SA" % s for s in CARTEIRA_IBRX]
 
-def cron_b3():
-    """
-    Funcao bastante rapida, porem usa muita memoria do Heroku (640Mb / 512Mb)
-    """
+def download_b3():
     # Limpa a Base
     Quotes.objects.filter(market='BOVESPA').delete()
     download_hquotes(ibrx_tickers)
 
-    # Limpa a Base
-    PairStats.objects.filter(market='BOVESPA').delete()
+def download_binance():
+    Quotes.objects.filter(market='BINANCE').delete()
+    download_hquotes_binance()
+
+def cron_b3_fast():
+    """
+    Funcao bastante rapida, porem usa muita memoria do Heroku (640Mb / 512Mb)
+    """
     with Pool(2) as p:
         bulk_list = p.starmap(b3_producer, enumerate(gera_pares(ibrx_tickers)))
 
     # Grava dados no Banco
+    PairStats.objects.filter(market='BOVESPA').delete()
     PairStats.objects.bulk_create(bulk_list)
 
     # Telegram
@@ -51,13 +55,9 @@ def cron_b3_memory():
     """
     Funcao que prioriza o uso limitado da memoria.
     """
-    # Limpa a Base
-    Quotes.objects.filter(market='BOVESPA').delete()
-    download_hquotes(ibrx_tickers)
 
     # Limpa a Base
     PairStats.objects.filter(market='BOVESPA').delete()
-
     bulk_list = []
     for idx, pair in enumerate(gera_pares(ibrx_tickers)):
         obj = b3_producer(idx, pair)
@@ -70,34 +70,32 @@ def cron_b3_memory():
             del bulk_list
             bulk_list = []
 
-    # Grava dados no Banco
+    # Grava dados no Banco o restante
     PairStats.objects.bulk_create(bulk_list)
 
     # Telegram
     send_msg()
 
-def cron_binance():
+def cron_binance_memory():
 
-    # Limpa a Base
-    Quotes.objects.filter(market='BINANCE').delete()
-    download_hquotes_binance()
-
-    # Limpa a Base
     PairStats.objects.filter(market='BINANCE').delete()
     bulk_list = []
     for idx, pair in enumerate(gera_pares(BINANCE_FUTURES)):
         obj = binance_producer(idx, pair)
         bulk_list.append(obj)
 
-    # Grava dados no Banco
+        if len(bulk_list) > 2000:
+            # Grava dados no Banco
+            PairStats.objects.bulk_create(bulk_list)
+            # Libera a memória
+            del bulk_list
+            bulk_list = []
+
+    # Grava dados no Banco o restante
     PairStats.objects.bulk_create(bulk_list)
 
 if __name__ == '__main__':
-    dfunc = {
-        'b3': cron_b3_memory,
-        'binance': cron_binance,
-    }
-    args = dfunc.keys()
-    if len(sys.argv) == 3:
-        args = sys.argv[2]
-    [dfunc[f]() for f in args]
+    download_b3()
+    download_binance()
+    cron_b3_fast()
+    cron_binance_memory()
